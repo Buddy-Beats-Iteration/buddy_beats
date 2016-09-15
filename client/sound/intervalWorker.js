0acd8dd5a1813d846ed47481ae2a7787839dbf91
Bryan Yee <yee.bryan.c@gmail.com>
2016-09-14 17:44:54 -0700

    Replace magic numbers with constants

--- client/sound/intervalWorker.js
+++ client/sound/intervalWorker.js
@@ -1,12 +1,15 @@
-
+const MS_TO_BPM_RATIO = 60000;
+const EIGTH_NOTES_RATIO = 0.5;
 let timerID;
+
 self.onmessage = function(e) {
 	clearInterval(timerID);
 	
   if (e.data.type === 'start' || e.data.type === 'changeBpm') {
+    const beatLength = MS_TO_BPM_RATIO * EIGTH_NOTES_RATIO / e.data.bpm; // length of each beat in milliseconds
     self.postMessage('tick');
-    timerID = setInterval(()=> {
+    timerID = setInterval(() => {
       self.postMessage('tick');
-    }, 60 / e.data.bpm * 1000 / 2)
+    }, beatLength)
   }
 }
